4762c050f2b04b9e580fbcdf2e22d0a351ae0281
jessLijie <wong.lijie1105@gmail.com>
2024-06-17 12:01:19 +0800

    Add svm_model.pkl using Git LFS

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model and vectorizer
+svm_model = joblib.load('svm_model.pkl')
+tfidf_vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_text = request.form['input_text']
+    preprocessed_input = tfidf_vectorizer.transform([input_text])
+    
+    # Convert sparse matrix to dense
+    preprocessed_input_dense = preprocessed_input.toarray()
+    
+    prediction = svm_model.predict(preprocessed_input_dense)
+    probability = svm_model.predict_proba(preprocessed_input_dense)
+
+    result = {
+        'prediction': int(prediction[0]),
+        'probability': probability[0].tolist()
+    }
+
+    # Set the depression level and message based on the probability
+    depression_level = ""
+    message = ""
+    # if prediction[0] == 1:
+    prob = probability[0][1]
+
+    if prob < 0.1:
+        depression_level = "No Depression"
+        message = "No depression detected. Continue maintaining a healthy lifestyle. If you ever feel down or need support, don't hesitate to talk to someone you trust."
+    elif 0.1 < prob < 0.4:
+        depression_level = "Level 1: Mild"
+        message = "It seems you might be experiencing mild symptoms of depression. It's a good idea to talk to a friend or family member about how you're feeling. Engaging in regular physical activity, maintaining a balanced diet, and practicing mindfulness can also help improve your mood."
+    elif 0.41 < prob < 0.7:
+        depression_level = "Level 2: Moderate"
+        message = "It appears you may be experiencing moderate symptoms of depression. Consider seeking support from a mental health professional. Therapy or counseling can be very beneficial. Additionally, try to engage in activities that you enjoy and that help you relax."
+    elif prob >= 0.7:
+        depression_level = "Level 3: Severe"
+        message = "Your responses indicate that you may be experiencing severe symptoms of depression. It is highly recommended to seek immediate help from a mental health professional. Don't hesitate to reach out to friends or family members for support as well."
+
+    result['depression_level'] = depression_level
+    result['recommendation'] = message
+  
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
